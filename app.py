616f22ab379669a323c8aa3d99934710db9c8fec
EJDonato <eltonjamesdonato05@gmail.com>
2025-04-27 08:49:04 +0800

    test

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask, jsonify, render_template, request
 
+# test function only, delete later
+def login_test(email, password):
+    test_email = "airam@gmail.com"
+    test_password = "abc123"
+
+    if test_email == email and test_password == password:
+        acc_name = "AiramJeanReglos"
+        return [True, acc_name]
+    else:
+        return [False, "accNotFound"]
 
 class FlaskApp():
     def __init__(self):
@@ -10,6 +20,28 @@ class FlaskApp():
         def index():
             return render_template("index.html")
         
+        @self.app.route("/login")
+        def login():
+            return render_template("login.html")
+        
+        @self.app.route("/homepage/<user>")
+        def homepage(user):
+            return render_template("home_page.html")
+        
+        # this is where python fetches login info
+        @self.app.route("/login/login_info", methods=["POST"])
+        def check_login_info():
+            data = request.json
+            email = data.get("email")
+            password = data.get("password")
+
+            check = login_test(email, password)
+            result = {"result":check[0],
+                      "acc_name":check[1]}
+
+            return jsonify(result)
+
+        
 
     def run(self, **kwargs):
         self.app.run(**kwargs)
@@ -19,3 +51,6 @@ app = FlaskApp().app
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
---
+++ static/index.js
@@ -0,0 +1,13 @@
+
+
+// Front Page Customer Button
+const customerbtn = document.getElementById("customerbtn")
+customerbtn.addEventListener("click", gotologin);
+
+function gotologin() {
+    window.location.href = "/login";
+}
+
+
+
+
